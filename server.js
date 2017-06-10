c1f8aae72df32c9e62c0b8ee965276057b55548f
Sol Laudon <blaudon1@gmail.com>
2017-06-10 16:54:54 -0600

    sl: add skillcard route

--- server.js
+++ server.js
@@ -22,6 +22,35 @@ switch (app.get('env')) {
   default:
 }
 
+app.use(bodyParser.json());
+
+app.use((req, res, next) => { //checks for json format
+  if (/json/.test(req.get('Accept'))) {
+    return next();
+  }
+
+  res.sendStatus(406);
+});
+
+const skillboard = require('./routes/skillboard');
+
+app.use(skillboard);
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((err, _req, res, _next) => {
+  if (err.output && err.output.statusCode) {
+    return res
+      .status(err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+      .send(err.message);
+  }
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
 app.listen(port, () => {
   if (app.get('env') !== 'test') {
     // eslint-disable-next-line no-console
