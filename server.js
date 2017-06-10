24ced73d1880074b3aeea639fe58b006a87e0667
Sol Laudon <blaudon1@gmail.com>
2017-06-10 16:32:38 -0600

    sl: fleshed out server file

--- server.js
+++ server.js
@@ -1,2 +1,32 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const port = process.env.PORT || 3000;
+
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+    app.use(morgan('short'));
+    break;
+
+  default:
+}
+
+app.listen(port, () => {
+  if (app.get('env') !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', port);
+  }
+});
+
+module.exports = app;
