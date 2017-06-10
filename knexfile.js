949461050e7b70b689fae9fb30b003b45221f976
jmrigali <jmrigali@ymail.com>
2017-06-10 17:32:45 -0600

    jr: add express.static

--- knexfile.js
+++ knexfile.js
@@ -7,9 +7,10 @@ module.exports = {
     connection: 'postgre://localhost/skilbuild'
   },
 
+
   production: {
     client: 'pg',
-    connection: process.env.DATABSE_URL
+    connection: process.env.DATABASE_URL
   }
-  
+
 };
--- server.js
+++ server.js
@@ -1,2 +1,32 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+    app.use(morgan('short'));
+    break;
+
+  default:
+}
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+app.get('/', (req, res, next)=>{
+  res.render('index');
+});
+
+app.listen(3000, function(){
+  console.log("listening on port 3000");
+});
+
+
+module.exports = app;
