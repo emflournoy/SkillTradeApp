a91e5b3fc979e1da05a5177cc22d8d1f2015e140
Elizabeth Flournoy <bflournoy@gmail.com>
2017-06-12 17:23:55 -0600

    jr: incomplete framework

--- public/javascript/skillsManager.js
+++ public/javascript/skillsManager.js
@@ -1,15 +1,43 @@
 $(document).ready(function() {
 
 
+//API CALL FUNCTION TO LOAD ALL CATEGORIES==========
+$.getJSON('http://localhost:3000/skillManager')
+    .done((catsEnvArr) => {
+      createDropdowns(catsEnvArr[0], 'catItem', 'categoryDropdown');
+      createDropdowns(catsEnvArr[1], 'envItem', 'environmentDropdown');
+    })
+    .fail(() => {
+      console.log('not working');
+    });
+
+//POPULATE FORM DROPDOWNS==========================
+function createDropdowns(arr, idName, appendTo){
+  for(let i=0; i<arr.length; i++){
+    let $clonedItem = $(`#${idName}`).clone();
+    $clonedItem.removeAttr("id");
+    let indId= 'item' + `${arr.id}`;
+    $clonedItem.attr("id", indId);
+    $clonedItem.text(`${arr[i].type}`);
+    $(`#${appendTo}`).append($clonedItem);
+    console.log('working');
+  };
+}
+
+
+
+
 $('#skillSubmit').on('click', (event)=> {
   let skill_card_obj = {
     title: $('#title-Box').val(),
     description: $('#descriptionTextBox').val(),
     photo: $('#photo-Box').val(),
-    user_id: '',//need to access userID
+    user_id: 'dog',//need to access userID
     categories_id: $('#category-results').val(),//need to access
-    environment_id: $('#environment-results').val()//need to access
+    environment_id: $('#environment-results').val(),//need to access
+    contact: $('#contact-results').val()
   };
+  console.log(skill_card_obj);
 //Option 1
   $.post( 'http://localhost:3000/skillManager/' + skill_card_obj.user_id, skill_card_obj)
   .done((data) => {
