663217740663683413f089ab21214c706209e5db
jmrigali <jmrigali@ymail.com>
2017-06-13 15:40:41 -0600

    changed the oauth to http instead of https as it errors out on the browser

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,54 +1,5 @@
 $(document).ready(function(){
 
-// checkLoginState();
-  // FB.getLoginStatus(function(response){
-  //     if(res.status != "connected") {
-  //       //have the button display "please log on"
-  //     } else {
-  //       //have button show "continue as facebook user"
-  //     }
-  //   });
-
-    //   console.log("clicked");
-    //   if(false) {   //this.html == "please log on"
-    //     FB.login(function(inResponse){
-    //       if (inResponse.status == 'connected'){
-    //       toggleVisible();
-    //       generateFriends(inResponse.authResponse.userID);
-    //       generateMe();
-    //     $.get('/skillManager')
-    //     }
-    //     },{scope: 'public_profile'})
-    //   } else {
-    //
-    //    $.get('/skillManager')
-    //   }
-    // });
-
-
-  //
-  //
-  // $('#login').click(function(){
-  //   FB.login(function(inResponse){
-  //     if (inResponse.status == 'connected'){
-  //     toggleVisible();
-  //     generateFriends(inResponse.authResponse.userID);
-  //     generateMe();
-  //   }
-  // },{scope: 'public_profile'});
-  // });
-  //
-
-  //
-  // $("#logout").click(function(){
-  //   FB.logout(function(response) {
-  //     console.log(response);// Person is now logged out
-  //   });
-  // })
-  // //
-  // // //
-  // checkLoginState();
-
   const sbUserId= [];
 
   var loggedin;
@@ -71,7 +22,6 @@ $(document).ready(function(){
     var loginResponse;
     function checkLoginState() {
       FB.getLoginStatus(function(response) {
-        console.log(response, "response when checking if logged");
           if(response.status == 'connected' && response.status != undefined){
           loggedin = true;
           loginResponse = response.authResponse.userID;
@@ -84,10 +34,7 @@ $(document).ready(function(){
 
       });
     }
-    console.log(loggedin);
     if(loggedin){
-
-      //TODO check if they already have an account if not make one THEN redirect
 
       $.ajax({
         contentType: 'application/json',
@@ -97,20 +44,17 @@ $(document).ready(function(){
         dataType: 'json',
       })
       .done((user) => {
-        console.log(user);
+
       })
       .fail((err) => {
-        console.log('not working');
+
       });
       window.location.replace("html/skillsManager.html");
       return;
     }else if (!loggedin){
       FB.login(function(inResponse){
-        // checkLoginState();
-        console.log(inResponse.status);
         checkLoginState();
           if (inResponse.status == 'connected'){
-
             $.ajax({
               contentType: 'application/json',
               type: "POST",
@@ -119,31 +63,17 @@ $(document).ready(function(){
               dataType: 'json',
             })
             .done((user) => {
-              console.log(user);
             })
             .fail(() => {
-              console.log('not working');
             });
 
           }
       },{scope: 'public_profile'})
-        // window.location.replace("html/skillsManager.html")
       return;
-      //TODO check if they already have an account if not make one THEN redirect
-
 
   } else {
-    console.log('not sure');
+
   }
   });
-
-    // function checkLoginState() {
-    //   FB.getLoginStatus(function(response) {
-    //     if(response.status == "connected"){
-    //       loggedin = true
-    //     }
-    //     console.log(loggedin);
-    //   });
-    // }
 
 });
