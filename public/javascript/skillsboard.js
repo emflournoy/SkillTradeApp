0347d47f969bc1e160f083a685a7ed2a136fc572
jmrigali <jmrigali@ymail.com>
2017-06-14 15:53:58 -0600

    grabbing target ids on cards for modals

--- public/javascript/skillsboard.js
+++ public/javascript/skillsboard.js
@@ -19,6 +19,8 @@ $.getJSON('http://localhost:3000/skillboard')
     .done((allCards) => {
       console.log(allCards);
       createTradeCard(allCards);
+      // createModal(allCards);
+        // $('#skillCardModal').modal('hide');
     })
     .fail(() => {
       console.log('not working');
@@ -26,6 +28,7 @@ $.getJSON('http://localhost:3000/skillboard')
 
 //FUNCTION TO CREATE SKILL CARDS========================
 function createTradeCard(arr){
+
   arr.map((e)=>{
     let $tradeCard = $("#blankTradeCard").clone();
     $tradeCard.removeAttr("id");
@@ -39,30 +42,34 @@ function createTradeCard(arr){
     let $text = $tradeCard.find("#cardText");
       $text.text(e.description);
     $('#tradeCardsContainer').append($tradeCard);
-    $('.skillCardBtn').on('click', (event)=>{
-      console.log(event.target)
-      var id = e.id;
-      // console.log(id);
-      event.stopPropagation();
-      // createModal(arr, id);
-
-      $('.modal').modal('show');
-    });
   }); //END OF MAP
+
+  $('.skillCardBtn').on('click', (event)=>{
+    console.log($(event.target).closest('.col-sm-4').attr('id'))
+    $('.modal').modal('show');
+  });
 }
-
-  function createModal(arr, id){
-
-  var modalContent = {}
-
-    arr.filter(function(e){
-      if (e.id === id){
-        modalContent = e;
-      }
-      return modalContent
-    });
-    // console.log(modalContent);
-  }
+  //POPULATE MODAL========================
+function createModal(arr){
+  arr.map((e)=>{
+    let $modalCard = $(".sManagerBody").clone();
+    $modalCard.removeAttr("id");
+    let indId= 'Modal ' + e.id;
+    $modalCard.attr("id", indId);
+    // Put in content from api call
+    let $photo = $modalCard.find('.pic')
+      $photo.attr('src', e.photo);
+    let $description = $modalCard.find(".desc");
+      $description.text(e.title);
+    let $category = $modalCard.find(".cat");
+      $category.text(e.cat);
+    let $environment = $modalCard.find(".env");
+      $environment.text(e.env);
+    let $contactInfo = $modalCard.find(".contactInfo");
+      $contactInfo.text(e.contact);
+    // $('.modal-body').append($modalCard);
+  });
+}
 
 
 //END DOC READY
